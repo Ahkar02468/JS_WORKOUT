b7c4e05e6cf9ce14f9bb36557bb15efe156628f0
aksb <Ahkar02468>
2023-08-24 23:18:27 +0630

    solve capitalized problem

---
+++ 01-console/09-string/script.js
@@ -0,0 +1,5 @@
+let myName = "ahkar";
+// let s = myName.__proto__;
+const x = myName.toLocaleUpperCase();
+
+console.log(x);
---
+++ 01-console/10-string-capitalize/script.js
@@ -0,0 +1,5 @@
+let word = 'developer';
+const firstWord = word[0].toUpperCase();
+const secWord = word.substring(1);
+
+console.log(`${firstWord}${secWord}`);
