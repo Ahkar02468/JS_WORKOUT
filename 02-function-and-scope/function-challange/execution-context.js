b453f5387b69b3d14cd00756b2b45fcdf5d7aa98
aksb <Ahkar02468>
2023-08-30 21:45:15 +0630

    done function challance

---
+++ 02-function-and-scope/function-challange/execution-context.js
@@ -0,0 +1,10 @@
+const x = 100;
+const y = 200;
+function getSum(num1, num2){
+    return num1 + num2;
+}
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(23, 45);
+
+console.log(sum1, sum2);
--- 02-function-and-scope/function-challange/script.js
+++ 02-function-and-scope/function-challange/script.js
@@ -1,16 +1,16 @@
 //first challange
-// const ferToCel = fer => (Math.round((fer - 32) * 5 / 9));
-// console.log(`The temperature is ${ferToCel(180)} \xB0C.`);
+const ferToCel = fer => (Math.round((fer - 32) * 5 / 9));
+console.log(`The temperature is ${ferToCel(180)} \xB0C.`);
 
 //second challange
-// const minMax = (arr) => {
-//     const newObj = {};
-//     newObj.min = Math.min(...arr);
-//     newObj.max = Math.max(...arr);    
-//     return newObj;
-// }
+const minMax = (arr) => {
+    const newObj = {};
+    newObj.min = Math.min(...arr);
+    newObj.max = Math.max(...arr);    
+    return newObj;
+}
 
-// console.log(minMax([1, 2, 10, 3, 4, 5]));
+console.log(minMax([1, 2, 10, 3, 4, 5]));
 
 //Challenge 3 IIFE
 ((width, length) => {
