4e56ed88d011b5707721b87f974698e8ba9caf5e
aksb <Ahkar02468>
2023-08-30 22:03:47 +0630

    test iteration for in and of loop

--- 03-iteration-array/script.js
+++ 03-iteration-array/script.js
@@ -9,11 +9,20 @@ const users = [{name: 'aksb'}, {name: 'john doe'}, {name: 'jane doe'}];
 //     console.log(user.name);
 // }
 
-
-
 //Loop over string
-const letter = 'Hello';
-for(const lets of letter){
-    console.log(lets);
-} 
+// const letter = 'Hello';
+// for(const lets of letter){
+//     console.log(lets);
+// } 
+
+//For in loop
+const colorObjects = {
+    name1: 'red',
+    name2: 'green',
+    name3: 'blue'
+}
+
+for(const key in colorObjects){
+    console.log(key, colorObjects[key]);
+}
 
