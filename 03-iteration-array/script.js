3f9fb715aff0628665995f5f8a75e5a9d79f57da
aksb <Ahkar02468>
2023-08-30 21:55:58 +0630

    for of loop

---
+++ 03-iteration-array/script.js
@@ -0,0 +1,19 @@
+const newArr = ['dog', 'cat', 'apple', 'orange'];
+const users = [{name: 'aksb'}, {name: 'john doe'}, {name: 'jane doe'}];
+
+// for(const arr of newArr){
+//     console.log(arr);
+// }
+
+// for(const user of users){
+//     console.log(user.name);
+// }
+
+
+
+//Loop over string
+const letter = 'Hello';
+for(const lets of letter){
+    console.log(lets);
+} 
+
