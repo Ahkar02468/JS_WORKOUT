b7ea89cfc3d98fe50b345e948b33c4beb7fb2667
Ahkar02468 <arkarshwebaw@gmail.com>
2023-10-17 00:02:19 +0630

    chage callback to promise

--- 08-asynchronus-js/callback/script.js
+++ 08-asynchronus-js/callback/script.js
@@ -8,11 +8,19 @@ let posts = [
     {title: 'Post two', body: 'This is post two'}
 ];
 
-function createPosts(post, cb){
-    setTimeout(() => {
-        posts.push(post);
-        cb();
-    }, 2000);
+function createPosts(post){
+    return new Promise((resolve, reject) => {
+        let errFlag = true;
+        if(!errFlag){
+            setTimeout(() => {
+                posts.push(post);
+                resolve();
+            }, 2000);
+        }else{
+            reject("Something went wrong!!");
+        }
+       
+    })
    
 }
 
@@ -28,8 +36,13 @@ function getPosts(){
 }
 
 // getPosts();
-createPosts({title:'Post three', body: 'This is post three.'}, getPosts);
-createPosts({title:'Post foour', body: 'This is post four.'}, getPosts);
+createPosts({title:'Post three', body: 'This is post three.'}).then(getPosts)
+.catch((err) => {
+    const div = document.createElement('h3');
+    div.innerHTML = `<strong>${err}</strong>`;
+    document.querySelector('#post').appendChild(div);
+});
+// createPosts({title:'Post foour', body: 'This is post four.'}, getPosts);
 
 //dont pass parnathese when call a callback function
 document.querySelector('button').addEventListener('click', toggle)
