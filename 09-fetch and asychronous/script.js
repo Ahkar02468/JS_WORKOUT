98e464bb1ea9a03a04d8797141c2ebded444660b
Ahkar02468 <arkarshwebaw@gmail.com>
2023-10-20 10:23:24 +0630

    test fetching with jsonplaceholder

--- 09-fetch and asychronous/script.js
+++ 09-fetch and asychronous/script.js
@@ -1,7 +1,17 @@
-fetch('./movie.json')
-    .then((data) => {
-        return data.json();
+function fetchData({title, body}){
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            token: 'abc123'
+        }
     })
-    .then(data => {
-        console.log(data);
-    });
+    .then((response) => response.json())
+    .then(data => console.log(data));
+}
+
+fetchData({title: 'My Post', body: 'This is body.'});
