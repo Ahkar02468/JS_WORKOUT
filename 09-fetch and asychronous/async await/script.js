ea8cb97ea4ac37c5101b9e508cf506797828acfc
Ahkar02468 <arkarshwebaw@gmail.com>
2023-10-23 23:48:03 +0630

    test async & await function

---
+++ 09-fetch and asychronous/async await/script.js
@@ -0,0 +1,31 @@
+const promise = new Promise((resolve, reject) => {
+     setTimeout(() => {
+          resolve({name: 'John', age: 40})
+     }, 2000)
+});
+// promise.then((res) => console.log(res));
+
+
+async function getPromise(){
+     const response = await promise;
+     console.log(response);
+}
+
+// getPromise();
+
+//fetch with async and await
+async function getusers(){
+     const user = await fetch('https://jsonplaceholder.typicode.com/users');
+     const response = await user.json();
+     console.log(response);
+}
+//arrow async
+async function getPosts(){
+     const post = await fetch('https://jsonplaceholder.typicode.com/posts');
+     const response = await post.json();
+     console.log(response);
+}
+
+
+// getusers();
+getPosts();
