646a0f4a284e621990dfcb48184b6dfdf3175063
Ahkar02468 <arkarshwebaw@gmail.com>
2023-11-13 22:07:31 +0630

    add methods to prototype chain

---
+++ 11-OOP/add method to proto chain/script.js
@@ -0,0 +1,16 @@
+function Rectangular(name, height, width){
+     this.name = name;
+     this.height = height;
+     this.width = width;
+}
+
+Rectangular.prototype.area = function () {
+     return this.width * this.height;
+}
+
+const rec1 = new Rectangular('Rect 1', 10, 100);
+console.log(rec1.area());
+
+const rec2 = new Rectangular('Rect 2', 30, 500);
+console.log(rec2.area());
+console.log(rec1);
