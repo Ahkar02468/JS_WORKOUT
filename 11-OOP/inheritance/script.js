4acb926e9b754d92cc909cde7e911e20d9286e6f
Ahkar02468 <arkarshwebaw@gmail.com>
2023-11-13 23:45:25 +0630

    inheritance

---
+++ 11-OOP/inheritance/script.js
@@ -0,0 +1,37 @@
+class Shape {
+     constructor(name){
+          this.name = name;
+     }
+
+     logName(){
+          console.log(`Shape Name: ` + this.name);
+     }
+}
+
+class Rectangle extends Shape{
+     constructor(name, width, height){
+          super(name);
+          this.width = width;
+          this.height = height;
+     }
+     //polymorphism
+     logName(){
+          console.log(`Rectangle name: ` + this.name);
+         }
+}
+
+class Circle extends Shape{
+     constructor(name, radius){
+          super(name);
+          this.radius = radius;
+     }
+     // logName(){
+     //      console.log(`Circle name: ` + this.name);
+     //     }
+}
+
+const rect1 = new Rectangle('Rect 1');
+rect1.logName();
+
+const cir1 = new Circle('Cir 1');
+cir1.logName();
